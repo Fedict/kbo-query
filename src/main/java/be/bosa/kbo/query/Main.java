81b3bc6479591e1a478e9c2a810420239e7f4442
Bart Hanssens <bart.hanssens@bosa.fgov.be>
2020-04-05 18:07:02 +0200

    Additional docs Signed-off-by:Bart Hanssens <bart.hanssens@bosa.fgov.be>

--- src/main/java/be/bosa/kbo/query/Main.java
+++ src/main/java/be/bosa/kbo/query/Main.java
@@ -1,13 +1,34 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2020, Bart Hanssens <bart.hanssens@bosa.fgov.be>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 package be.bosa.kbo.query;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.SetMultimap;
+
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 
@@ -16,8 +37,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -26,41 +47,37 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
- *
- * @author Bart.Hanssens
+ * Custom query for mapping CBE open data
+ * 
+ * @author Bart Hanssens
  */
 public class Main {
-	private static Path input;
-	private static Path kboDir;
-
 	/**
 	 * 
 	 * @param args
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		input = Paths.get(args[0]);
-		kboDir = Paths.get(args[1]);
+		if (args.length < 3) {
+			System.err.println("usage: input-file directory-kbo-csv output-file");
+			System.exit(-1);
+		}
+
+		String input = args[0];
+		String kboDir = args[1];
+		String output = args[2];
 		
 		List<Row> rows = readInput(input);
 		Set<String> toCheck = getNrsToCheck(rows);
-
-		/*
-		Multimap<String,String> sites = readSites(toCheck, kboDir, "establishment.csv");
-		for(Entry<String,String> entry : sites.entries()) {
-			System.err.println(entry.getKey() + " " + entry.getValue());
-		}
-		*/
 
 		Map<String,String> naceNL = readCodes(kboDir, "code.csv", "NL");
 		Map<String,String> naceFR = readCodes(kboDir, "code.csv", "FR");
@@ -70,19 +87,20 @@ public class Main {
 		Multimap<String,String> hActMain = readActivities(toCheck, kboDir, "activity.csv", "RSZ001", "MAIN");
 		Multimap<String,String> hActSeco = readActivities(toCheck, kboDir, "activity.csv", "RSZ001", "SECO");
 
-		writeOutput(kboDir, "output.xlsx", rows, vatActMain, vatActSeco, hActMain, hActSeco, naceNL, naceFR);
+		writeOutput(kboDir, output, rows, vatActMain, vatActSeco, hActMain, hActSeco, naceNL, naceFR);
 	}
 	
 	/**
+	 * Read input file (XLSX) and keep the rows in memory
 	 * 
-	 * @param inFile
-	 * @return
+	 * @param inFile input file
+	 * @return list of rows
 	 * @throws IOException 
 	 */
-	private static List<Row> readInput(Path inFile) throws IOException {
+	private static List<Row> readInput(String inFile) throws IOException {
 		List<Row> rows = new ArrayList<>();
 		
-		try (InputStream is = Files.newInputStream(inFile)) {
+		try (InputStream is = Files.newInputStream(Paths.get(inFile))) {
 			XSSFWorkbook myWorkBook = new XSSFWorkbook(is); 
 			XSSFSheet mySheet = myWorkBook.getSheetAt(0);
 			Iterator<Row> rowIterator = mySheet.iterator();
@@ -93,13 +111,23 @@ public class Main {
 		}
 		return rows;
 	}
+
+
 	/**
+	 * Write result to output file (XLSX)
 	 * 
-	 * @param inFile
-	 * @return
+	 * @param outDir output directory
+	 * @param file output file
+	 * @param rows rows
+	 * @param vatActMain VAT main activity
+	 * @param vatActSeco VAT secondary activity
+	 * @param hActMain NSSO main activity
+	 * @param hActSeco NSSO secondary activity
+	 * @param naceNL NACE codes Dutch
+	 * @param naceFR NACE code French
 	 * @throws IOException 
 	 */
-	private static void writeOutput(Path outFile, String file, List<Row> rows, 
+	private static void writeOutput(String outDir, String file, List<Row> rows, 
 			Multimap<String,String> vatActMain,Multimap<String,String> vatActSeco,
 			Multimap<String,String> hActMain,Multimap<String,String> hActSeco,
 			Map<String,String> naceNL, Map<String,String> naceFR) throws IOException {
@@ -111,7 +139,8 @@ public class Main {
 
 		for (int i = 0; i < rows.size(); i++) {
 			Row newRow = sheet.createRow(i);
-			
+
+			// copy existing rows			
 			for (int j = 0; j < 9; j++) {
 				Cell cell = rows.get(i).getCell(j);
 				Cell newCell = newRow.createCell(j);
@@ -124,6 +153,8 @@ public class Main {
 					}
 				}
 			}
+			
+			// add new columns about activities
 			Cell compcell = rows.get(i).getCell(4);
 			if (compcell != null) {
 				String company = compcell.getStringCellValue();
@@ -211,16 +242,17 @@ public class Main {
 			}
 		}
 
-		try (OutputStream os = Files.newOutputStream(Paths.get(outFile.toString(), file))) {
+		try (OutputStream os = Files.newOutputStream(Paths.get(outDir, file))) {
 			wb.write(os);
 		}
 		wb.close();
 	}
 	
 	/**
+	 * Get company numbers from excel
 	 * 
 	 * @param rows
-	 * @return 
+	 * @return set of company numbers
 	 */
 	private static Set<String> getNrsToCheck(List<Row> rows) {
 		Set<String> nrs = new HashSet<>(2048);
@@ -237,11 +269,22 @@ public class Main {
 		
 		return nrs;
 	}
-	
-	private static Multimap<String,String> readActivities(Set<String> nrs, Path dir, String file, 
+
+	/**
+	 * Get the VAT/NSSO activities for a list of companies
+	 * 
+	 * @param nrs companies to check
+	 * @param dir input directory
+	 * @param file activity file
+	 * @param actGroup activity group (VAT or NSSO)
+	 * @param importance main or secondary activity
+	 * @return
+	 * @throws IOException 
+	 */
+	private static Multimap<String,String> readActivities(Set<String> nrs, String dir, String file, 
 																String actGroup, String importance) throws IOException {
 		SetMultimap<String,String> multi = HashMultimap.create();
-		try (	Reader reader = Files.newBufferedReader(Paths.get(dir.toString(), file));
+		try (	Reader reader = Files.newBufferedReader(Paths.get(dir, file));
 				CSVReader csvReader = new CSVReader(reader)) {
 			String[] row;
             while ((row = csvReader.readNext()) != null) {
@@ -255,9 +298,18 @@ public class Main {
 		return multi;
 	}
 
-	private static Map<String, String> readCodes(Path dir, String file, String lang) throws IOException {
+	/**
+	 * Read NACE code table and create a map of NACE numbers and label in specific language
+	 * 
+	 * @param dir directory
+	 * @param file file to read
+	 * @param lang language
+	 * @return map of code and label
+	 * @throws IOException 
+	 */
+	private static Map<String, String> readCodes(String dir, String file, String lang) throws IOException {
 		HashMap<String,String> codes = new HashMap<>(2048);
-		try (	Reader reader = Files.newBufferedReader(Paths.get(dir.toString(), file));
+		try (	Reader reader = Files.newBufferedReader(Paths.get(dir, file));
 				CSVReader csvReader = new CSVReader(reader)) {
 			String[] row;
             while ((row = csvReader.readNext()) != null) {
@@ -271,9 +323,17 @@ public class Main {
 		return codes;
 	}
 
-	private static Multimap<String,String> readSites(Set<String> nrs, Path dir, String file) throws IOException {
+	/**
+	 * 
+	 * @param nrs
+	 * @param dir
+	 * @param file
+	 * @return
+	 * @throws IOException 
+	 */
+	private static Multimap<String,String> readSites(Set<String> nrs, String dir, String file) throws IOException {
 		SetMultimap<String,String> multi = HashMultimap.create();
-		try (	Reader reader = Files.newBufferedReader(Paths.get(dir.toString(), file));
+		try (	Reader reader = Files.newBufferedReader(Paths.get(dir, file));
 				CSVReader csvReader = new CSVReader(reader)) {
 			String[] row;
             while ((row = csvReader.readNext()) != null) {
